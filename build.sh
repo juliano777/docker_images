5bc27a52ea505d24ef8fa33983daa2cf07fa57be
Juliano Atanazio <juliano777@gmail.com>
2020-05-12 17:14:20 -0300

    build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+export TAG="${1}"
+
 docker image prune -f
 
 docker image build \
-    -t juliano777/postgres:latest\
+    -t juliano777/${TAG}\
     --compress\
     --no-cache\
     --squash\
